03a3bdc09811497ddb8e70757a24515d7d7a574d
JOSE-ZAJAMA <jose.zajama@gmail.com>
2023-07-12 23:39:11 -0300

    mapa

--- tkinter_mapview.py
+++ tkinter_mapview.py
@@ -8,11 +8,11 @@ class MapApp(tk.Tk):
         self.title("Map App")
         self.geometry("800x600")
         
-        # Crea un widget de mapa
+        # widget de mapa
         self.map = mapview.MapView(self)
         self.map.pack(fill=tk.BOTH, expand=True)
 
-        # Establece las coordenadas y el nivel de zoom inicial
+        # coordenadas y el nivel de zoom inicial
         self.map.set_center(40.7128, -74.0060)  # Ejemplo: Nueva York
         self.map.set_zoom(10)  # Ejemplo: nivel de zoom 10
 
