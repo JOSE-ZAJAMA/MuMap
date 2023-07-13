5ef7b07195a01eded0a5c75258462e6ddac6713d
JOSE-ZAJAMA <jose.zajama@gmail.com>
2023-07-13 18:31:28 -0300

    modificacion de indice

--- main.py
+++ main.py
@@ -2,24 +2,19 @@ import tkinter as tk
 import customtkinter
 import json
 
-# CustomTkinter colores
-
-customtkinter.set_appearance_mode("System")
-customtkinter.set_default_color_theme("blue")
-
-# Dimensiones de la app
-
-app = customtkinter.CTk()
-app.geometry("720x480")
+# Definir los colores personalizados
+purple_dark = "#2F242C"
+gray_light = "#E5E5E5"
+yellow = "#E6D884"
+green_light = "#A1A892"
 
 eventos_widgets = []  # Widgets para los eventos actuales
 
+# Definir la función mostrar_indice_eventos
 def mostrar_indice_eventos():
-    # Widgets para mostrar los eventos en formato JSON
-    # Eliminar los widgets de eventos anteriores
     for widget in eventos_widgets:
         widget.destroy()
-
+    
     try:
         with open("data/eventos.json", "r") as file:
             eventos_data = json.load(file)
@@ -30,65 +25,70 @@ def mostrar_indice_eventos():
         evento_frame = tk.Frame(app)
         evento_frame.pack()
 
-        estadio_label = tk.Label(evento_frame, text=f"Lugar: {evento['estadio']}")
+        estadio_frame = tk.Frame(evento_frame)
+        estadio_frame.pack()
+
+        estadio_label = tk.Label(estadio_frame, text=f"Lugar: {evento['estadio']}")
         estadio_label.pack()
 
         artista_label = tk.Label(evento_frame, text=f"Artista: {evento['artista']}")
         artista_label.pack()
 
-        genero_label = tk.Label(evento_frame, text=f"Género: {evento['genero']}")
-        genero_label.pack()
+        ver_button = tk.Button(evento_frame, text="Ver Detalles", command=lambda id=evento['id']: mostrar_detalle_evento(id))
+        ver_button.pack()
 
-        ubicacion_label = tk.Label(evento_frame, text=f"Ubicación: {evento['ubicacion']}")
-        ubicacion_label.pack()
-
-        fecha_label = tk.Label(evento_frame, text=f"Fecha de evento: {evento['fecha']}")
-        fecha_label.pack()
-        
-        horaInicio_label = tk.Label(evento_frame, text=f"Hora de inicio del evento: {evento['horaInicio']}")
-        horaInicio_label.pack()
-
-        horaFin_label = tk.Label(evento_frame, text=f"Hora de cierre del evento: {evento['horaFin']}")
-        horaFin_label.pack()
-
-        descripcion_label = tk.Label(evento_frame, text=f"Descripción del evento: {evento['descripcion']}")
-        descripcion_label.pack()
-
-        cerrar_button = customtkinter.CTkButton(evento_frame, text="Cerrar ventana", command=evento_frame.destroy)
-        cerrar_button.pack()
-
-        # Almacena el frame del evento en la lista de widgets
         eventos_widgets.append(evento_frame)
 
+def mostrar_detalle_evento(evento_id):
+    detalle_frame = tk.Toplevel(app)
+    detalle_frame.title("Detalles del Evento")
+    detalle_frame.geometry("400x300")
+
+    try:
+        with open("data/eventos.json", "r") as file:
+            eventos_data = json.load(file)
+            evento = next((evento for evento in eventos_data if evento['id'] == evento_id), None)
+    except FileNotFoundError:
+        evento = None
+
+    if evento:
+        estadio_label = tk.Label(detalle_frame, text=f"Nombre: {evento['estadio']}")
+        estadio_label.pack()
+
+        artista_label = tk.Label(detalle_frame, text=f"Artista: {evento['artista']}")
+        artista_label.pack()
+
+        genero_label = tk.Label(detalle_frame, text=f"Género: {evento['genero']}")
+        genero_label.pack()
+
+        descripcion_label = tk.Label(detalle_frame, text=f"Descripción: {evento['descripcion']}")
+        descripcion_label.pack()
+
+        cerrar_button = tk.Button(detalle_frame, text="Cerrar ventana", command=detalle_frame.destroy)
+        cerrar_button.pack()
+    else:
+        mensaje_label = tk.Label(detalle_frame, text="No se encontró información del evento.")
+        mensaje_label.pack()
+
 def button_function(section):
     if section == "indice_eventos":
         mostrar_indice_eventos()
 
-def mostrar_busqueda_eventos():
-    # Lugar para los nuevos widgets para realizar la búsqueda y mostrar los resultados
-    pass
+# Crear la ventana principal de la aplicación
+app = tk.Tk()
+app.geometry("720x480")
+app.title("♫ TITULO DE LA APP ♫")
 
-def mostrar_historial_eventos():
-    # Lugar para crear nuevos widgets para mostrar el historial en formato JSON
-    pass
-
-def button_function(section):
-    if section == "indice_eventos":
-        mostrar_indice_eventos()
-    elif section == "busqueda_eventos":
-        mostrar_busqueda_eventos()
-    elif section == "historial_eventos":
-        mostrar_historial_eventos()
-
-welcome_label = customtkinter.CTkLabel(master=app, text="Bienvenido a NOMBRE DE LA APP", font=("Roboto", 24))
+# Crear los widgets de la interfaz de usuario
+welcome_label = tk.Label(app, text="Bienvenido a NOMBRE DE LA APP", font=("Arial", 24), fg=purple_dark)
 welcome_label.place(relx=0.5, rely=0.1, anchor=tk.CENTER)
 
-button1 = customtkinter.CTkButton(master=app, text="Índice de Eventos", command=lambda: button_function("indice_eventos"))
+button1 = tk.Button(app, text="Índice de Eventos", command=lambda: button_function("indice_eventos"), bg=yellow, fg=purple_dark)
 button1.place(relx=0.5, rely=0.4, anchor=tk.CENTER)
-button2 = customtkinter.CTkButton(master=app, text="Búsqueda y Filtrado de Eventos", command=lambda: button_function("busqueda_eventos"))
+button2 = tk.Button(app, text="Búsqueda y Filtrado de Eventos", command=lambda: button_function("busqueda_eventos"), bg=yellow, fg=purple_dark)
 button2.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
-button3 = customtkinter.CTkButton(master=app, text="Historial de Eventos", command=lambda: button_function("historial_eventos"))
+button3 = tk.Button(app, text="Historial de Eventos", command=lambda: button_function("historial_eventos"), bg=yellow, fg=purple_dark)
 button3.place(relx=0.5, rely=0.6, anchor=tk.CENTER)
 
-app.title("TITULO DE LA APP")
+# Iniciar el bucle principal de la aplicación
 app.mainloop()
